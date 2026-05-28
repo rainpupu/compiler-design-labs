6c33887f746303cf581189e97facf8fff4190d37
rainy <150757289+rainpupu@users.noreply.github.com>
2026-05-28 11:30:26 +0800

    Register SLR(1) lab sidebar item

--- scanner/src/main.js
+++ scanner/src/main.js
@@ -3,5 +3,7 @@ import App from './AppI18n.vue'
 import './style.css'
 import './lr0-style.css'
 import './slr1-style.css'
+import { installSLR1Lab } from './lab4-patch.js'
 
 createApp(App).mount('#app')
+installSLR1Lab()
