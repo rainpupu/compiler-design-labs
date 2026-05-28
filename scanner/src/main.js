4dbff3927f4c6e5e7dca11245077e83112e1f200
rainy <150757289+rainpupu@users.noreply.github.com>
2026-05-28 18:06:11 +0800

    Install lab5 lab6 visual entry

--- scanner/src/main.js
+++ scanner/src/main.js
@@ -3,7 +3,10 @@ import App from './AppI18n.vue'
 import './style.css'
 import './lr0-style.css'
 import './slr1-style.css'
+import './lab56-style.css'
 import { installSLR1Lab } from './lab4-patch.js'
+import { installLab56 } from './lab56-patch.js'
 
 createApp(App).mount('#app')
 installSLR1Lab()
+installLab56()
