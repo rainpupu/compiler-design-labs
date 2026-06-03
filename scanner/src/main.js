0a77b93d1694ddb95fa35e5eb9e95a401050d1f9
rainy <150757289+rainpupu@users.noreply.github.com>
2026-06-03 18:04:56 +0800

    Install full visual compiler pipeline

--- scanner/src/main.js
+++ scanner/src/main.js
@@ -4,9 +4,12 @@ import './style.css'
 import './lr0-style.css'
 import './slr1-style.css'
 import './lab56-style.css'
+import './pipeline-style.css'
 import { installSLR1Lab } from './lab4-patch.js'
 import { installLab56 } from './lab56-patch.js'
+import { installPipelineLab } from './pipeline-patch.js'
 
 createApp(App).mount('#app')
 installSLR1Lab()
 installLab56()
+installPipelineLab()
